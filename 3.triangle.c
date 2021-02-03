7878331166e24c649657822d67f69fd4b89601b3
Thangavel V <76422033+ThangavelVds@users.noreply.github.com>
2021-02-03 22:08:57 +0530

    Add files via upload

---
+++ 3.triangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void up_stars(int line,int n)
+{
+    for(int space = 1 ; space <= n - line ; space++)
+    {
+        printf("  ");
+    }
+    for(int star = 1 ; star<=(2*line)-1 ; star++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+
+}
+void main()
+{
+    int n;
+    printf("\nEnter number of lines :");
+    scanf("%d",&n);
+    for(int i = 1 ; i<=n ; i++)
+    {
+        up_stars(i,n);
+    }
+}
